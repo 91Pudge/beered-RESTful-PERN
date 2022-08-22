26498c7eedf4a22d49a30b8cb8130fb7a598fe18
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-22 20:46:52 +0900

    Feat: Add post function

--- client/src/components/InputBeer.jsx
+++ client/src/components/InputBeer.jsx
@@ -1,25 +1,54 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./Input.css";
 
+// review_id SERIAL PRIMARY KEY,
+// beer_name VARCHAR(255),
+// brewery_name VARCHAR(255),
+// style VARCHAR(255),
+// descriptions VARCHAR(255)
+
 const InputBeer = () => {
+  const [beer_name, setBeer_name] = useState("");
+  const [brewery_name, setBrewery_name] = useState("");
+  const [style, setStyle] = useState("");
+  const [descriptions, setDescriptions] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = { beer_name, brewery_name, style, descriptions };
+    try {
+      await fetch("http://localhost:5000/beer", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+      window.location = "/";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Fragment>
       <h1>Beered.co.jp</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex-input">
           <div className="input-card">
             <article>
               <p>BEER NAME:</p>
-              <input></input>
+              <input
+                value={beer_name}
+                onChange={(e) => setBeer_name(e.target.value)}
+              ></input>
               <br />
               <p>BREWERY NAME:</p>
-              <input></input>
+              <input onChange={(e) => setBrewery_name(e.target.value)}></input>
               <br />
               <p>STYLE:</p>
-              <input></input>
+              <input onChange={(e) => setStyle(e.target.value)}></input>
               <br />
               <p>DESCRIPTION:</p>
-              <input></input>
+              <input onChange={(e) => setDescriptions(e.target.value)}></input>
               <br />
               <br />
               <button>Submit</button>
