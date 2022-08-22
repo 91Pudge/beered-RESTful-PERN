1df1fe9d04f86a7a1d3f2dcb8c614a22c5dba999
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-22 11:11:35 +0900

    Feat: Add post route

--- server/index.js
+++ server/index.js
@@ -2,21 +2,36 @@ const express = require("express");
 const app = express();
 const port = 5000;
 var cors = require("cors");
+const pool = require("./db");
 
 //middleware;
-app.express(express.json);
+app.use(express.json());
 app.use(cors());
 
 //post
-
-// get
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/beer", async (req, res) => {
+  try {
+    const { beer_name, brewery_name, style, descriptions } = req.body;
+    const newEntry = await pool.query(
+      "INSERT INTO drinks (beer_name, brewery_name, style, descriptions) VALUES ($1, $2, $3, $4) RETURNING *",
+      [beer_name, brewery_name, style, descriptions]
+    );
+    res.json(newEntry.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
 });
 //getall
+app.get;
+// get
+app.get("/beer", (req, res) => {
+  res.send("Hello World!");
+});
 //put(update)
+app.put;
 //delete
+app.delete;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(` server listening on port ${port}`);
 });
