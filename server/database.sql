00612ceb16cbf43903b9e3927f74e8312086c517
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-22 10:39:54 +0900

    Feat: create a database

---
+++ server/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE beer;
+CREATE TABLE drinks(
+    review_id SERIAL PRIMARY KEY,
+    beer_name VARCHAR(255),
+    brewery_name VARCHAR(255),
+    style VARCHAR(255),
+    descriptions VARCHAR(255)
+);
+
+
+
