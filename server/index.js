9ce46832d8a92224c4b55c521ee4dccc23c7c91f
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-22 10:16:03 +0900

    Feat: Add middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+var cors = require("cors");
 
+//mid
+
+dleware;
+app.express(express.json);
+app.use(cors());
+
+//post
+
+// get
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+//getall
+//put(update)
+//delete
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
