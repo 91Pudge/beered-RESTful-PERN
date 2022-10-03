77c74e0733e6b72fb6ad791cb26eb17db09c8b8e
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-03 21:01:01 +0900

    Fix: Change to edit code

--- db.js
+++ db.js
@@ -2,18 +2,24 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 // const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const devConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT
-};
-
-const proConfig = process.env.DATABASE_URL; //heroku addons
-
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT
+// };
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
+  user: "josh",
+  host: "localhost",
+  port: 5432,
+  database: "beer"
 });
+
+// const proConfig = process.env.DATABASE_URL; //heroku addons
+
+// const pool = new Pool({
+//   connectionString:
+//     process.env.NODE_ENV === "production" ? devConfig : proConfig
+// });
 
 module.exports = pool;
