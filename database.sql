c2fb9f6d3752fadbaaf88f4cb3f6407f238abee1
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-13 11:08:40 +0900

    Feat: Create user table

--- database.sql
+++ database.sql
@@ -7,5 +7,9 @@ CREATE TABLE drinks(
     descriptions VARCHAR(255)
 );
 
-
-
+CREATE TABLE users(
+user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+user_name VARCHAR(255) NOT NULL,
+user_email VARCHAR(255) NOT NULL,
+user_password VARCHAR(255) NOT NULL,
+);
