300f51aa2e191b06b1523d9adbf6b19f5ad28003
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-13 21:13:37 +0900

    Feat: Add links between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,58 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import ListBeer from "./components/ListBeer.jsx";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 function App() {
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <Fragment>
-      <ListBeer />;
+      <Router>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              !isAuthenticated ? (
+                <Login setAuth={setAuth} />
+              ) : (
+                <Navigate to="/dashboard" />
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              !isAuthenticated ? (
+                <Register setAuth={setAuth} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              isAuthenticated ? (
+                <Dashboard setAuth={setAuth} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route path="/listbeer" element={<ListBeer setAuth={setAuth} />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
