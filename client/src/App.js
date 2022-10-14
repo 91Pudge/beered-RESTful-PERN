a411f3e5bb6a50aa8e9e989078cedf20c4e7f932
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-14 14:17:03 +0900

    Feat: Create isauth function

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import ListBeer from "./components/ListBeer.jsx";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
@@ -14,6 +14,22 @@ function App() {
   const setAuth = (boolean) => {
     setIsAuthenticated(boolean);
   };
+  async function isAuth() {
+    try {
+      const response = await fetch("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+      const parseRes = await response.json();
+      console.log(parseRes);
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  useEffect(() => {
+    isAuth();
+  });
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
