d8337f1e1de1d8fd102c2c398ddfdd2392025118
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-16 22:29:40 +0900

    Feat: Create navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,72 @@
+const NavBar = ({ setAuth }) => {
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  return (
+    <div className="Nav">
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <a className="navbar-brand" href="/dashboard">
+          Beered
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <button className="w-20 ml-2" onClick={() => logOut()}>
+          logout
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/listbeer">
+                Review a beer <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Store
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
