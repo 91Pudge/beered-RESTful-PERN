e1703d22fa6c4a66e6e9b2476e4e375e6569addf
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-21 11:36:00 +0900

    Feat: Get users messages from db

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,10 +5,11 @@ const authorisation = require("../middleware/authorisation");
 
 router.get("/", authorisation, async (req, res) => {
   try {
-    const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
-      req.user
-    ]);
-    res.json(user.rows[0]);
+    const user = await pool.query(
+      "SELECT u.user_name, d.review_id, d.descriptions FROM users AS u LEFT JOIN drinks AS d ON u.user_id = d.user_id WHERE u.user_id = $1",
+      [req.user]
+    );
+    res.json(user.rows);
   } catch (error) {
     console.error(error.message);
     res.status(500).json("Server error");
