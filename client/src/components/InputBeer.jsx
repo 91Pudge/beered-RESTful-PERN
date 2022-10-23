00c3b47594c2cc9426c1aa0e863d3f788cdabb75
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-24 07:55:55 +0900

    Feat: Add headers to link user post  with user

--- client/src/components/InputBeer.jsx
+++ client/src/components/InputBeer.jsx
@@ -18,11 +18,17 @@ const InputBeer = () => {
     e.preventDefault();
     const body = { beer_name, brewery_name, style, descriptions };
     try {
-      await fetch("http://localhost:5000/beer", {
+      const myHeaders = new Headers();
+
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("token", localStorage.token);
+
+      await fetch("http://localhost:5000/dashboard/beers", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: myHeaders,
         body: JSON.stringify(body)
       });
+
       window.location.href = "/listbeer";
     } catch (error) {
       console.log(error);
