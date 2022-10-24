e28ca4a787171d8448bbfda6a333eca2ad2c5593
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-24 12:51:04 +0900

    Feat: Add user name to welcome message

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useState, useEffect } from "react";
 
 const NavBar = ({ setAuth }) => {
+  const [name, setName] = useState("");
+
   const logOut = () => {
     localStorage.removeItem("token");
     toast.error("Logged out");
     setAuth(false);
   };
 
+  const getProfile = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/dashboard", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+
+      const parseData = await res.json();
+      setName(parseData[0].user_name);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
-    <div className="mb-2">
-      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+    <div className="">
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning ">
         <a className="navbar-brand" href="/dashboard">
           Beered
         </a>
@@ -25,7 +46,6 @@ const NavBar = ({ setAuth }) => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
@@ -33,13 +53,19 @@ const NavBar = ({ setAuth }) => {
                 Review a beer <span className="sr-only">(current)</span>
               </a>
             </li>
-            <Link to="/login">
-              <button className="w-16 mt-2" onClick={() => logOut()}>
-                logout
-              </button>
-            </Link>
+            <li className="nav-item active">
+              <a className="nav-link" href="/user-reviews">
+                User Reviews <span className="sr-only">(current)</span>
+              </a>
+            </li>
           </ul>
         </div>
+        Welcome- {name}
+        <Link to="/login">
+          <button className="w-16 mt-2 ml-2" onClick={() => logOut()}>
+            logout
+          </button>
+        </Link>
       </nav>
     </div>
   );
