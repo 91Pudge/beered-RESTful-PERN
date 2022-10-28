e249415ba08fbaf9e052c1d279ba8ee075227d1c
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-10-28 18:24:21 +0900

    Chore: Adjustment to dev config

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = `postgresql://${process.env.PG_USER}${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT
+};
 const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool(
